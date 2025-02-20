19ce9d61ac6c03d4095fcff82cb1447c60264340
MachineFox <eduardokaminski05@gmail.com>
2025-02-19 22:09:41 -0300

    Update Mix anim.lua

--- Mix anim.lua
+++ Mix anim.lua
@@ -1,58 +1,86 @@
 local Players = game:GetService("Players")
 local plr = Players.LocalPlayer
 
-local function isR6(character)
-    return character:FindFirstChild("Torso") ~= nil and character:FindFirstChild("Right Leg") ~= nil
+local function isR15(character)
+    local humanoid = character:WaitForChild("Humanoid", 5)
+    if humanoid then
+        return humanoid.RigType == Enum.HumanoidRigType.R15
+    else
+        return false
+    end
 end
 
 local function runLocalAnimate(character)
-    if isR6(character) then
-        print("Character is R6, script will not run.")
+    if not isR15(character) then
+        print("Personagem é R6; o script não será executado.")
         return
     end
 
-    -- Aguardando o carregamento do 'Animate' e do 'Humanoid'
-    local Animate = character:WaitForChild("Animate")
-    local Humanoid = character:WaitForChild("Humanoid")
+    -- Aguardando o 'Animate' estar disponível
+    local Animate = character:WaitForChild("Animate", 5)
+    if not Animate then
+        warn("Script 'Animate' não encontrado no personagem.")
+        return
+    end
 
     Animate.Disabled = true
 
+    -- Aguardando o 'Humanoid' estar disponível
+    local humanoid = character:WaitForChild("Humanoid", 5)
+    if not humanoid then
+        warn("Humanoid não encontrado no personagem.")
+        return
+    end
+
     -- Parando qualquer animação em execução
-    local animtrack = Humanoid:GetPlayingAnimationTracks()
-    for _, track in pairs(animtrack) do
+    local animTracks = humanoid:GetPlayingAnimationTracks()
+    for _, track in ipairs(animTracks) do
         track:Stop()
     end
 
-    -- Aguardando o carregamento das animações
-    local idle = Animate:WaitForChild("idle")
-    local walk = Animate:WaitForChild("walk")
-    local run = Animate:WaitForChild("run")
-    local jump = Animate:WaitForChild("jump")
-    local climb = Animate:WaitForChild("climb")
-    local fall = Animate:WaitForChild("fall")
-    local swim = Animate:WaitForChild("swim")
-    local swimidle = Animate:WaitForChild("swimidle")
+    -- Aguardando as pastas de animação
+    local idle = Animate:WaitForChild("idle", 5)
+    local walk = Animate:WaitForChild("walk", 5)
+    local run = Animate:WaitForChild("run", 5)
+    local jump = Animate:WaitForChild("jump", 5)
+    local climb = Animate:WaitForChild("climb", 5)
+    local fall = Animate:WaitForChild("fall", 5)
+    local swim = Animate:WaitForChild("swim", 5)
+    local swimidle = Animate:WaitForChild("swimidle", 5)
+
+    -- Aguardando as animações individuais dentro de cada pasta
+    local idleAnim1 = idle:WaitForChild("Animation1", 5)
+    local idleAnim2 = idle:WaitForChild("Animation2", 5)
+    local walkAnim = walk:WaitForChild("WalkAnim", 5)
+    local runAnim = run:WaitForChild("RunAnim", 5)
+    local jumpAnim = jump:WaitForChild("JumpAnim", 5)
+    local climbAnim = climb:WaitForChild("ClimbAnim", 5)
+    local fallAnim = fall:WaitForChild("FallAnim", 5)
+    local swimAnim = swim:WaitForChild("Swim", 5)
+    local swimIdleAnim = swimidle:WaitForChild("SwimIdle", 5)
 
     -- Substituindo as animações
-    idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=10921117521"
-    idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=10921118894"
-    walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=10921111375"
-    run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=10921104374"
-    jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=10921322186"
-    climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=10921070953"
-    fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=10921321317"
-    swim.Swim.AnimationId = "http://www.roblox.com/asset/?id=10921079380"
-    swimidle.SwimIdle.AnimationId = "http://www.roblox.com/asset/?id=10921081059"
+    idleAnim1.AnimationId = "http://www.roblox.com/asset/?id=10921117521"
+    idleAnim2.AnimationId = "http://www.roblox.com/asset/?id=10921118894"
+    walkAnim.AnimationId = "http://www.roblox.com/asset/?id=10921111375"
+    runAnim.AnimationId = "http://www.roblox.com/asset/?id=10921104374"
+    jumpAnim.AnimationId = "http://www.roblox.com/asset/?id=10921322186"
+    climbAnim.AnimationId = "http://www.roblox.com/asset/?id=10921070953" -- Nova animação de escalada
+    fallAnim.AnimationId = "http://www.roblox.com/asset/?id=10921321317"
+    swimAnim.AnimationId = "http://www.roblox.com/asset/?id=10921079380" -- Animação de nadar
+    swimIdleAnim.AnimationId = "http://www.roblox.com/asset/?id=10921081059"
 
     Animate.Disabled = false
 end
 
 local function onCharacterAdded(character)
     print(character.Name .. " spawned!")
-    if isR6(character) then
-        print("Character is R6, script will not run.")
+
+    if not isR15(character) then
+        print("Personagem é R6; o script não será executado.")
         return
     end
+
     runLocalAnimate(character)
 end
 
